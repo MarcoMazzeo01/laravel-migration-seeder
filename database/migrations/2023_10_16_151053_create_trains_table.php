df0cea5a1bced7965bad3c8ff83b6f71f774b816
plastikSun <marcom2k01@gmail.com>
2023-10-16 17:24:42 +0200

    create: trains migration + columns

---
+++ database/migrations/2023_10_16_151053_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 30);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->time('departure', $precision = 0);
+            $table->time('arrival', $precision = 0);
+            $table->smallInteger('carriages');
+            $table->boolean('delayed');
+            $table->boolean('canceled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
